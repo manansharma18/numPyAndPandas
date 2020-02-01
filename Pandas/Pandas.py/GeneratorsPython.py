33e3b262e2ddd40f7fb515d9dd34aa9970ece0ca
Manan Sharma <manansharma@v1041-wn-rt-a-250-255.campus-dynamic.uwaterloo.ca>
2020-02-01 09:51:11 -0500

    pandas

--- Pandas/Pandas.py/GeneratorsPython.py
+++ Pandas/Pandas.py/GeneratorsPython.py
@@ -5,3 +5,10 @@ sq_iterator = (x**2 for x in range(10))  # this produces an iterator of squares
 
 print(sq_list)
 print(sq_iterator)
+
+try: 
+    x = 5/0
+except ZeroDivisionError:
+    print('Cannot divide by zero')
+finally:
+    print("lolz")
